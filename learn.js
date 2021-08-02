bd8f6c84ca936aa070084718dc0c0579669571df
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-08-02 17:19:46 +0600

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,34 @@
+
+
+// For Loop========================= Example With a Fan
+
+// for(initializer, condition, Increment/Decrement){
+
+// }
+
+
+
+
+for(var i=0;i<=100;++i){
+  
+   if(i===5) {
+
+   break
+
+   }//continue; 
+
+console.log(i);
+
+}
+
+console.log("BREAK");
+
+
+for(var i=100;i>0;i--){
+
+
+console.log(i);
+
+}
+
+
